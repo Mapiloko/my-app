ee616beacd141c885a0f9c1e2e429120ce1ad931
Mapiloko <93536735+Mapiloko@users.noreply.github.com>
2023-01-02 14:40:44 +0200

    commit test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import './App.css';
-// import React, {useEffect, useRef, useState } from 'react';
+import findIndex from "lodash/findIndex";
+// import startCase from "lodash/startCase";
+import React from 'react';
 
 function App() {
+
+
+  // Original array
+let array1 = [4, 2, 3, 1, 4, 2]
+  
+// Using lodash.findIndex
+let index = findIndex(array1, (e) => {
+    return e == 2;
+}, 2);
+  
+console.log("original Array: ", array1)
+console.log("index: ", index)
+  
+
 
   return (
     <div>
